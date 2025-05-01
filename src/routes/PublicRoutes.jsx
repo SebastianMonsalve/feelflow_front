6ddda4e169289a71ed1b913c1b3867e37d94301d
Sebastian Monsalve <sebastiandev16@gmail.com>
2025-05-01 03:08:31 -0500

    fix: Correct import path casing for LoginPage in PublicRoutes

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PublicLayout from "../Layouts/PublicLayout.jsx";
 import Welcome from "../Pages/PublicPages/Welcome.jsx";
-import LoginPage from "../pages/PublicPages/LoginPage.jsx";
+import LoginPage from "../Pages/PublicPages/LoginPage.jsx";
 import RegisterPage from "../pages/PublicPages/RegisterPage.jsx";
 
 const PublicRoutes = () => (
